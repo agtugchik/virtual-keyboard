235583be6fc7968d17e9525a54ebd706490c6e5a
agtugchik <agtugchik13@gmail.com>
2022-04-30 20:37:35 +0300

    feat: add language change

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,64 +1,15 @@
-import keyCodesToRu from './objects-and-arrays.js';
+import { keyCodesToRu, keyCodesToEn } from './objects-and-arrays.js';
 
-const keys = Object.keys(keyCodesToRu);
+const container = document.createElement('div');
+const name = document.createElement('h1');
+const textarea = document.createElement('textarea');
+const keyboard = document.createElement('div');
+const description = document.createElement('p');
+const keys = Object.keys(keyCodesToEn);
 
-const addKeys = (target) => {
-  keys.forEach((element) => {
-    const key = document.createElement('div');
-    key.classList.add(`${element}`, 'key');
-    if (element === 'ArrowUp' || element === 'ArrowLeft'
-      || element === 'ArrowDown' || element === 'ArrowRight'
-      || element === 'ControlLeft' || element === 'ControlRight'
-      || element === 'MetaLeft') {
-      if (element === 'ArrowUp') key.innerHTML = '&#8593;';
-      if (element === 'ArrowLeft') key.innerHTML = '&#8592;';
-      if (element === 'ArrowDown') key.innerHTML = '&#8595;';
-      if (element === 'ArrowRight') key.innerHTML = '&#8594;';
-      if (element === 'ControlLeft' || element === 'ControlRight') key.innerHTML = 'Ctrl';
-      if (element === 'MetaLeft') key.innerHTML = 'Win';
-    } else {
-      key.innerHTML = `${keyCodesToRu[element]}`;
-    }
+let language = 'Ru';
 
-    target.append(key);
-  });
-};
-
-const mouseListener = (target) => {
-  target.addEventListener('mousedown', (event) => {
-    event.target.classList.add('active');
-  });
-
-  document.addEventListener('mouseup', () => {
-    keys.forEach((element) => {
-      document.querySelector(`.${element}`).classList.remove('active');
-    });
-  });
-};
-
-const keyboardListener = () => {
-  document.addEventListener('keydown', (event) => {
-    if (event.code === 'CapsLock') {
-      document.querySelector(`.${event.code}`).classList.toggle('active');
-    } else {
-      if (event.code === 'Tab') event.preventDefault();
-      document.querySelector(`.${event.code}`).classList.add('active');
-    }
-  });
-  document.addEventListener('keyup', (event) => {
-    if (event.code !== 'CapsLock') {
-      document.querySelector(`.${event.code}`).classList.remove('active');
-    }
-  });
-};
-
-const init = () => {
-  const container = document.createElement('div');
-  const name = document.createElement('h1');
-  const textarea = document.createElement('textarea');
-  const keyboard = document.createElement('div');
-  const description = document.createElement('p');
-
+const createBasestructure = () => {
   container.classList.add('container');
   document.body.append(container);
 
@@ -76,7 +27,97 @@ const init = () => {
   description.innerHTML = 'The keyboard was created in the windows operating system'
     + '<br>Keys to switch layout: left shift + left alt';
   container.append(description);
+};
 
+const addKeys = (target) => {
+  keys.forEach((element) => {
+    const key = document.createElement('div');
+    key.classList.add(`${element}`, 'key');
+    if (element === 'ArrowUp' || element === 'ArrowLeft'
+      || element === 'ArrowDown' || element === 'ArrowRight'
+      || element === 'ControlLeft' || element === 'ControlRight'
+      || element === 'MetaLeft') {
+      if (element === 'ArrowUp') key.innerHTML = '&#8593;';
+      if (element === 'ArrowLeft') key.innerHTML = '&#8592;';
+      if (element === 'ArrowDown') key.innerHTML = '&#8595;';
+      if (element === 'ArrowRight') key.innerHTML = '&#8594;';
+      if (element === 'ControlLeft' || element === 'ControlRight') key.innerHTML = 'Ctrl';
+      if (element === 'MetaLeft') key.innerHTML = 'Win';
+    } else {
+      key.innerHTML = `${language === 'En' ? keyCodesToEn[element] : keyCodesToRu[element]}`;
+    }
+
+    target.append(key);
+  });
+};
+
+const switchLanguages = () => {
+  const condition = document.querySelector('.ShiftLeft').classList.contains('active')
+    && document.querySelector('.AltLeft').classList.contains('active');
+  if (condition) {
+    if (language === 'En') language = 'Ru';
+    else language = 'En';
+  }
+  keys.forEach((element) => {
+    const key = document.querySelector(`.${element}`);
+    if (element === 'ArrowUp' || element === 'ArrowLeft'
+      || element === 'ArrowDown' || element === 'ArrowRight'
+      || element === 'ControlLeft' || element === 'ControlRight'
+      || element === 'MetaLeft') {
+      if (element === 'ArrowUp') key.innerHTML = '&#8593;';
+      if (element === 'ArrowLeft') key.innerHTML = '&#8592;';
+      if (element === 'ArrowDown') key.innerHTML = '&#8595;';
+      if (element === 'ArrowRight') key.innerHTML = '&#8594;';
+      if (element === 'ControlLeft' || element === 'ControlRight') key.innerHTML = 'Ctrl';
+      if (element === 'MetaLeft') key.innerHTML = 'Win';
+    } else {
+      key.innerHTML = `${language === 'En'
+        ? keyCodesToEn[element] : keyCodesToRu[element]}`;
+    }
+  });
+};
+
+const mouseListener = (target) => {
+  target.addEventListener('mousedown', (event) => {
+    if (event.code === 'CapsLock') {
+      event.target.classList.toggle('active');
+    } else {
+      event.target.classList.toggle('active');
+    }
+  });
+
+  document.addEventListener('mouseup', () => {
+    keys.forEach((element) => {
+      if (element !== 'CapsLock') {
+        document.querySelector(`.${element}`).classList.remove('active');
+      }
+    });
+  });
+};
+
+const keyboardListener = () => {
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'CapsLock') {
+      document.querySelector(`.${event.code}`).classList.toggle('active');
+    } else {
+      if (event.code === 'Tab'
+        || event.code === 'AltLeft'
+        || event.code === 'AltRight') event.preventDefault();
+      document.querySelector(`.${event.code}`).classList.add('active');
+    }
+  });
+  document.addEventListener('keyup', (event) => {
+    const condition = document.querySelector('.ShiftLeft').classList.contains('active')
+      && document.querySelector('.AltLeft').classList.contains('active');
+    if (condition) switchLanguages();
+    if (event.code !== 'CapsLock') {
+      document.querySelector(`.${event.code}`).classList.remove('active');
+    }
+  });
+};
+
+const init = () => {
+  createBasestructure();
   addKeys(keyboard);
   mouseListener(keyboard);
   keyboardListener();
